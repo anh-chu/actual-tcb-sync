e98174a38a4c160a5583dcdb33f3690a1c38359a
Anh Chu <me@anhdchu.com>
2023-06-13 21:46:28 +0700

    get access token for TCB from session storage

--- src/tcb/index.ts
+++ src/tcb/index.ts
@@ -1,17 +1,50 @@
+import { getCurrentTab } from "../util/chrome";
+
 let tcb: string;
 chrome.storage.sync.get(["tcbUrl", "actualUrl"], (items) => {
   tcb = items.tcbUrl;
 });
 
+async function getAccessToken() {
+  const activeTab = await getCurrentTab();
+  const tabId = activeTab?.id as number;
+
+  function _() {
+    try {
+      return sessionStorage?.access_token as string;
+    } catch (err) {
+      console.error("Error occured in getting sessionStorage", err);
+    }
+  }
+
+  const injectionResults = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: _,
+  });
+  return injectionResults[0]?.result;
+}
+
 export async function getArrangements(): Promise<Arrangement[]> {
+  const accessToken = await getAccessToken();
+  const authHeader = {
+    Authorization: `Bearer ${accessToken}`,
+  };
   const url = `${tcb}/api/arrangement-manager/client-api/v2/productsummary/context/arrangements?businessFunction=Product%20Summary&resourceName=Product%20Summary&privilege=view`;
-  const r = await fetch(url);
+  const r = await fetch(url, {
+    headers: authHeader,
+  });
   return await r.json();
 }
 
 export async function getGoals(): Promise<Goal[]> {
+  const accessToken = await getAccessToken();
+  const authHeader = {
+    Authorization: `Bearer ${accessToken}`,
+  };
   const url = `${tcb}/api/savings-goal-dis/client-api/v1/savings-goal`;
-  const r = await fetch(url);
+  const r = await fetch(url, {
+    headers: authHeader,
+  });
   return await r.json();
 }
 
@@ -21,7 +54,13 @@ export async function getTransactions(
   from: number = 0,
   count: number = 200
 ): Promise<Transaction[]> {
+  const accessToken = await getAccessToken();
+  const authHeader = {
+    Authorization: `Bearer ${accessToken}`,
+  };
   const url = `${tcb}/api/transaction-manager/client-api/v2/transactions?bookingDateGreaterThan=${minDate}&bookingDateLessThan=${maxDate}&from=${from}&size=${count}`;
-  const r = await fetch(url);
+  const r = await fetch(url, {
+    headers: authHeader,
+  });
   return await r.json();
 }
