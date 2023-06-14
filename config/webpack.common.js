c141c140a084b1f054ad04c8511829ca381b2c14
Anh Chu <me@anhdchu.com>
2023-06-14 12:35:29 +0700

    reformatting

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,7 +2,7 @@
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 const PATHS = require('./paths');
 
@@ -14,71 +14,71 @@ const IMAGE_TYPES = /\.(png|jpe?g|gif|svg)$/i;
 // Whenever user creates an extension, CLI adds `webpack.common.js` file
 // in template's `config` folder
 const common = {
-  output: {
-    // the build folder to output bundles and assets in.
-    path: PATHS.build,
-    // the filename template for entry chunks
-    filename: '[name].js',
-  },
-  stats: {
-    all: false,
-    errors: true,
-    builtAt: true,
-    assets: true,
-    excludeAssets: [IMAGE_TYPES],
-  },
-  module: {
-    rules: [
-      // Check for TypeScript files
-      {
-        test: /\.ts$/,
-        use: ['ts-loader'],
-      },
-      // Help webpack in understanding CSS files imported in .js files
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      // Check for images imported in .js files and
-      {
-        test: IMAGE_TYPES,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name].[ext]',
+    output: {
+        // the build folder to output bundles and assets in.
+        path: PATHS.build,
+        // the filename template for entry chunks
+        filename: '[name].js',
+    },
+    stats: {
+        all: false,
+        errors: true,
+        builtAt: true,
+        assets: true,
+        excludeAssets: [IMAGE_TYPES],
+    },
+    module: {
+        rules: [
+            // Check for TypeScript files
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
             },
-          },
+            // Help webpack in understanding CSS files imported in .js files
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            // Check for images imported in .js files and
+            {
+                test: IMAGE_TYPES,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name].[ext]',
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    resolve: {
+        // Help webpack resolve these extensions in order
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        // Copy static assets from `public` folder to `build` folder
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: '**/*',
+                    context: 'public',
+                },
+            ],
+        }),
+        // Extract CSS into separate files
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
     ],
-  },
-  resolve: {
-    // Help webpack resolve these extensions in order
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-    // Copy static assets from `public` folder to `build` folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: '**/*',
-          context: 'public',
-        },
-      ],
-    }),
-    // Extract CSS into separate files
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    })
-  ],
 };
 
 module.exports = common;
